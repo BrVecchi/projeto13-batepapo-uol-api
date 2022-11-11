1207ef4adf6f60e664925b53f28132437abd10f7
BrVecchi <bno.vecchi@gmail.com>
2022-11-11 17:09:54 -0300

    add get and post from messages route

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ dotenv.config();
 const userSchema = joi.object({
     name: joi.string().required()
   });
+const messageSchema = joi.object({
+    to: joi.string().required(),
+    text: joi.string().required(),
+    type: joi.string().valid('message', 'private_message')
+})
+
 
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db;
@@ -23,8 +29,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// rota "/participantes"
-app.get('/participantes', async (res, req) => {
+// "/participants" route
+app.get('/participants', async (req, res) => {
     try{
         const participants = await participatnsCollection.find({}).toArray()
         res.status(200).send(participants)
@@ -33,7 +39,7 @@ app.get('/participantes', async (res, req) => {
         res.sendStatus(500)
     }
 })
-app.post('/participantes', async (res, req) => {
+app.post('/participants', async (req, res) => {
     const user = req.body
     const validation = userSchema.validate(user, { abortEarly: false });
 
@@ -44,7 +50,8 @@ app.post('/participantes', async (res, req) => {
     }
 
     try{
-        const nameValidation = await participatnsCollection.findOne({name: user.name})
+        const {name} = req.body
+        const nameValidation = await participatnsCollection.findOne({name: name})
         if(nameValidation) {
             res.sendStatus(409)
             return
@@ -58,4 +65,55 @@ app.post('/participantes', async (res, req) => {
     }
 })
 
+// "/messages" route
+
+app.post('/messages', async (req, res) => {
+    const {to, text, type} = req.body
+    const {from} = req.headers
+    const message = {
+        to,
+        text,
+        type,
+        from
+    }
+    const messageValidation = {
+        to,
+        text,
+        type
+    }
+
+    const validation = messageSchema.validate(messageValidation, { abortEarly: false });
+    console.log(validation)
+    if (validation.error) {
+        const errors = validation.error.details.map((detail) => detail.message);
+        res.status(422).send(errors)
+        return
+    }
+
+    try{
+        const fromValidation = await participatnsCollection.findOne({name: from})
+        if(!fromValidation) {
+            res.sendStatus(422)
+            return
+        }
+
+        await messagesCollection.insertOne({from: from, to: to, text: text, type: type, time: dayjs().format('HH:MM:ss')})
+        res.sendStatus(201)
+    }catch (error) {
+        console.error(error)
+        res.sendStatus(500)
+    }
+})
+
+app.get('/messages', async (req, res) => {
+    try {
+        const messages = await messagesCollection.find({}).toArray()
+        res.status(200).send(messages)
+    } catch(error){
+        console.error(error)
+        res.sendStatus(500)
+    }
+})
+
 app.listen(5000);
+console.log("Rodando na porta 5000")
